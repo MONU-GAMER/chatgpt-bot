17a19ba6f76da036076f7ef0ff24e824d258abdf
MONU-GAMER <84650793+MONU-GAMER@users.noreply.github.com>
2023-03-13 11:49:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,8 @@ app = Client(
     name="app",
     api_id=3755621,
     api_hash="86a96994dce114bb1b4bd4167c87a62b",
-    bot_token=5074293303:AAGQX4Fe3W0viiYqG5Vy7RJzmRr6GAcO-fw,
+    bot_token="5074293303:AAGQX4Fe3W0viiYqG5Vy7RJzmRr6GAcO-fw",
+    openai_api="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",
     plugins=plugins,
     workers=300,
 )
